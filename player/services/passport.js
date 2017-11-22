d306dcdbb6bf150088e230030750e0239a1f1ec7
Christopher Kim <christopherkim@Christophers-MacBook-Pro-2.local>
2017-11-21 21:19:26 -0800

    passport jwt strategy is working to protect unauthorized access

--- player/services/passport.js
+++ player/services/passport.js
@@ -1,1 +1,35 @@
 const passport = require('passport')
+const ExtractJwt = require('passport-jwt').ExtractJwt
+const JwtStrategy = require('passport-jwt').Strategy
+
+const User = require('../models/user')
+const config = require('../config')
+
+/* 
+ *  grab the jwt token from an incoming request and authorization header, 
+ *  it will decrypt it and if it's valid token, its gonna grab the user from the db 
+ *  and add it to our request object for use.
+ *  it it's invalid, passport will send back 401 error which is unauthorize error
+ */
+
+ var jwtOptions = {
+     secretOrKey: config.secret,
+     //grabs jwt token from request header
+     jwtFromRequest: ExtractJwt.fromHeader('authorization')
+ }
+
+ // payload (unencrypted token)
+ var jwtStrategy = new JwtStrategy (jwtOptions, function(payload, done) {
+    User.findById(payload.sub, function(err, user) {
+        if (err) {
+            return done(err, false)
+        }
+        if (user) {
+            done(null, user)
+        } else {
+            done(null, false)
+        }
+    })
+ })
+
+ passport.use(jwtStrategy);
--- player/services/router.js
+++ player/services/router.js
@@ -1,14 +1,19 @@
-const AuthController = require('../controllers/auth_controller');
-var router = require('express').Router();
+const passport = require('passport')
+
+const AuthController = require('../controllers/auth_controller')
+const passportService = require('./passport')
+
+var requireAuth = passport.authenticate('jwt', {session: false})
+var router = require('express').Router()
 
 function protected(req, res, next) {
-    res.send('Here is the secret!');
+    res.send('Here is the secret!')
 }
 
 router.route('/protected')
-    .get(protected);
+    .get(requireAuth, protected)
 
 router.route('/signup')
-    .post(AuthController.signup);
+    .post(AuthController.signup)
 
 module.exports = router
