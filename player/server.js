79b600cc1fc6b8329bde12ca57abc3e0b8e4139c
bennyficial <benny.u.lee@gmail.com>
2017-11-30 21:41:34 -0800

    upload

--- player/server.js
+++ player/server.js
@@ -1,52 +1,50 @@
-const express = require("express");
-const morgan = require('morgan');
-const bodyParser = require('body-parser')
-const path = require("path");
-const mongoose = require('mongoose');
+const express = require('express'),
+bodyParser = require("body-parser"),
+Customers = require("./client/models/Customers"),
+mongoose = require("mongoose"),
+mongoUrl = "mongodb://localhost/week18day2",
+path = require("path"),
+PORT = process.env.PORT || 3001;
+app = express();
 
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-var router = require('./services/router');
-
-let MONGO_URL
-const MONGO_LOCAL_URL = 'mongodb://localhost/songbox'
-
-if (process.env.MONGODB_URI) {
-	mongoose.connect(process.env.MONGODB_URI)
-	MONGO_URL = process.env.MONGODB_URI
-} else {
-	mongoose.connect(MONGO_LOCAL_URL) // local mongo url
-	MONGO_URL = MONGO_LOCAL_URL
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
 }
 
-const db = mongoose.connection
-db.on('error', err => {
-	console.log(`There was an error connecting to the database: ${err}`)
-})
-db.once('open', () => {
-	console.log(
-		`You have successfully connected to your mongo database: ${MONGO_URL}`
-	)
-})
-
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-
-app.use(morgan('combined'));
+app.use(express.static('client'))
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json({type: "application/vnd.api+json"}));
 
-app.use('/v1', router);
+mongoose.Promise = Promise;
+mongoose.connect(mongoUrl);
 
+app.get("/api/saved", function (req, res) {
+  
+  Customers
+      .find({}).sort({date:-1})
+      .exec(function (err, doc) {
+  
+          if (err) {
+              console.log(err);
+          } else {
+              res.send(doc);
+          }
+      });
+  });
 
+  const db = mongoose.connection;
+  
+  db.on("error", error => console.log("Database Error:", error));
+  
+  db.once("open", () => console.log("Mongoose connection successful."));
+
+require('./controllers/songController.js')(app)
 // Send every request to the React app
-// Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, './client/build/index.html'))
+})
 
-app.listen(PORT, function() {
-  console.log(`🌎 ==> Server now on port ${PORT}!`);
-});
+app.listen(PORT, function () {
+  console.log(`🌎 ==> Server now on port ${PORT}!`)
+})
